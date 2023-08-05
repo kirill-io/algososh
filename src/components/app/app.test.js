80a58100ed9c1c06596fa02e17c8edbd3e31fd8c
Kirill Begey <kirill.begey@mail.ru>
2023-08-05 22:46:29 +0300

    fix: исправлен тест компонента App

---
+++ src/components/app/app.test.js
@@ -0,0 +1,8 @@
+import { render, screen } from "@testing-library/react";
+import App from "./app";
+
+test("Компонент App отрендерился", () => {
+  render(<App />);
+  const appComponent = screen.getByTestId('appComponent');
+  expect(appComponent).toBeInTheDocument();
+});
