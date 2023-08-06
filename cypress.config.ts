169b85aca85d0e53b771420c2e8ed6319dcd70d5
Kirill Begey <kirill.begey@mail.ru>
2023-08-06 19:53:10 +0300

    feat: установка start-server-and-test, настройка cypress

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,9 +1,9 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
+  viewportWidth: 1440,
+  viewportHeight: 900,
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    baseUrl: 'http://localhost:3000/'
   },
 });
--- cypress/e2e/is-routing.spec.cy.js
+++ cypress/e2e/is-routing.spec.cy.js
@@ -1,54 +1,54 @@
 describe('Тестирование переходов по страницам', () => {
   before(() => {
-    cy.visit('http://localhost:3000/');
+    cy.visit('/');
     cy.wait(1000);
   });
 
   it('Проверка корректности перехода на страницу recursion', () => {
-    cy.visit('http://localhost:3000/recursion');
+    cy.visit('/recursion');
     cy.contains('Строка');
     cy.wait(1000);
-    cy.visit('http://localhost:3000/');
+    cy.visit('/');
     cy.wait(1000);
   });
 
   it('Проверка корректности перехода на страницу fibonacci', () => {
-    cy.visit('http://localhost:3000/fibonacci');
+    cy.visit('/fibonacci');
     cy.contains('Последовательность Фибоначчи');
     cy.wait(1000);
-    cy.visit('http://localhost:3000/');
+    cy.visit('/');
     cy.wait(1000);
   });
 
   it('Проверка корректности перехода на страницу sorting', () => {
-    cy.visit('http://localhost:3000/sorting');
+    cy.visit('/sorting');
     cy.contains('Сортировка массива');
     cy.wait(1000);
-    cy.visit('http://localhost:3000/');
+    cy.visit('/');
     cy.wait(1000);
   });
 
   it('Проверка корректности перехода на страницу stack', () => {
-    cy.visit('http://localhost:3000/stack');
+    cy.visit('/stack');
     cy.contains('Стек');
     cy.wait(1000);
-    cy.visit('http://localhost:3000/');
+    cy.visit('/');
     cy.wait(1000);
   });
 
   it('Проверка корректности перехода на страницу queue', () => {
-    cy.visit('http://localhost:3000/queue');
+    cy.visit('/queue');
     cy.contains('Очередь');
     cy.wait(1000);
-    cy.visit('http://localhost:3000/');
+    cy.visit('/');
     cy.wait(1000);
   });
 
   it('Проверка корректности перехода на страницу list', () => {
-    cy.visit('http://localhost:3000/list');
+    cy.visit('/list');
     cy.contains('Связный список');
     cy.wait(1000);
-    cy.visit('http://localhost:3000/');
+    cy.visit('/');
     cy.wait(1000);
   });
 });
