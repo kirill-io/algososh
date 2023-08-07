4c98812be473d20d8d223c0c712ce3f4b4c27ffc
Kirill Begey <kirill.begey@mail.ru>
2023-08-07 18:08:31 +0300

    feat: создание E2E-теста страницы разворота строки

--- cypress/e2e/is-routing.spec.cy.js
+++ cypress/e2e/is-routing.spec.cy.js
@@ -1,54 +1,39 @@
-describe('Тестирование переходов по страницам', () => {
+describe('Тестирование переходов по страницам:', () => {
   before(() => {
     cy.visit('/');
-    cy.wait(1000);
+  });
+
+  afterEach(() => {
+    cy.visit('/');
   });
 
   it('Проверка корректности перехода на страницу recursion', () => {
     cy.visit('/recursion');
-    cy.contains('Строка');
-    cy.wait(1000);
-    cy.visit('/');
-    cy.wait(1000);
+    cy.url().should('include', '/recursion');
   });
 
   it('Проверка корректности перехода на страницу fibonacci', () => {
     cy.visit('/fibonacci');
-    cy.contains('Последовательность Фибоначчи');
-    cy.wait(1000);
-    cy.visit('/');
-    cy.wait(1000);
+    cy.url().should('include', '/fibonacci');
   });
 
   it('Проверка корректности перехода на страницу sorting', () => {
     cy.visit('/sorting');
-    cy.contains('Сортировка массива');
-    cy.wait(1000);
-    cy.visit('/');
-    cy.wait(1000);
+    cy.url().should('include', '/sorting');
   });
 
   it('Проверка корректности перехода на страницу stack', () => {
     cy.visit('/stack');
-    cy.contains('Стек');
-    cy.wait(1000);
-    cy.visit('/');
-    cy.wait(1000);
+    cy.url().should('include', '/stack');
   });
 
   it('Проверка корректности перехода на страницу queue', () => {
     cy.visit('/queue');
-    cy.contains('Очередь');
-    cy.wait(1000);
-    cy.visit('/');
-    cy.wait(1000);
+    cy.url().should('include', '/queue');
   });
 
   it('Проверка корректности перехода на страницу list', () => {
     cy.visit('/list');
-    cy.contains('Связный список');
-    cy.wait(1000);
-    cy.visit('/');
-    cy.wait(1000);
+    cy.url().should('include', '/list');
   });
 });
