9d370e5fbd6e363ec67e66822880ca8cde5cd0a6
Kirill Begey <kirill.begey@mail.ru>
2023-08-10 18:01:11 +0300

    fix: удален тест компонента App

--- src/components/app/app.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import App from "./app";
-
-test("Компонент App отрендерился", () => {
-  render(<App />);
-  const appComponent = screen.getByTestId("appComponent");
-  expect(appComponent).toBeInTheDocument();
-});
